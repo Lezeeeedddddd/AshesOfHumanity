269873198b32f091a74eb2bdbc0c467e17486aca
Maniame <maniame.traore@epita.fr>
2025-12-13 14:38:58 +0100

    ajouts

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pygame
 pygame.init()
 
+#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#pygame.display.set_caption("Ashes Of Humanity")
+
+
 pygame.display.set_mode((800,600))#taille de la fenêtre
 pygame.display.set_caption("Ashes Of Humanity")#Nom du jeu
 
+#boucle du jeu
 running = True
 while running :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 #Pour que la fenêtre ne se ferme pas
 pygame.quit()
---
+++ player.py
@@ -0,0 +1,11 @@
+import pygame
+#Les objets (mouvement)
+
+class joueur(pygame.sprite.Sprite):#Sprite = element graphique du jeu (ici, le joueur)
+    def __init__(self):
+        super().__init__()
+
+
+        def touches_utilises(self):
+            pressed = pygame.key.get_pressed()
+
